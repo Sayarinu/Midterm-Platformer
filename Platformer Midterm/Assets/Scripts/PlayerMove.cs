928639c9f7d094fceff50e7fc781d469c85d68b6
Nick Pham <hebenzudoaa@gmail.com>
2022-10-12 01:46:57 -0400

    ground detection

--- Platformer Midterm/Assets/Scripts/PlayerMove.cs
+++ Platformer Midterm/Assets/Scripts/PlayerMove.cs
@@ -16,13 +16,26 @@ public class PlayerMove : MonoBehaviour
     float friction=0.15f;
     [SerializeField]
     float accel=0.2f;
+
+    [SerializeField]
+    private LayerMask plats;
+    [SerializeField]
+    float extraHeight = 0.1f;
+
+    [SerializeField]
+    float sideBuffer = 0.001f;
+
+    Color rayColor=Color.green;
     private PlayerInput input; 
+
+    private BoxCollider2D boxcollider;
     
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        boxcollider = GetComponent<BoxCollider2D>();
         input = new PlayerInput();
         input.Player.Enable();
         input.Player.jump.performed += Jump;
@@ -32,6 +45,14 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(isGrounded()){
+            rayColor = Color.green; 
+        }else{
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(boxcollider.bounds.center + new Vector3(boxcollider.bounds.extents.x-sideBuffer,0),Vector2.down*(boxcollider.bounds.extents.y + extraHeight), rayColor);
+        Debug.DrawRay(boxcollider.bounds.center - new Vector3(boxcollider.bounds.extents.x-sideBuffer,0),Vector2.down*(boxcollider.bounds.extents.y + extraHeight), rayColor);
+        Debug.DrawRay(boxcollider.bounds.center - new Vector3(boxcollider.bounds.extents.x-sideBuffer,boxcollider.bounds.extents.y + extraHeight),Vector2.right*(boxcollider.bounds.extents.x-sideBuffer)*2, rayColor);
         float val = input.Player.movement.ReadValue<float>();
         
         if(xspeed<speed && (speed*-1)<xspeed){
@@ -70,8 +91,14 @@ public class PlayerMove : MonoBehaviour
         
     }
 
+    private bool isGrounded(){
+        RaycastHit2D ray = Physics2D.BoxCast(boxcollider.bounds.center,boxcollider.bounds.size-new Vector3(sideBuffer,0,0),0f,Vector2.down,extraHeight,plats);
+        
+       
+        return ray.collider!=null;
+    }
     public void Jump(InputAction.CallbackContext context){
-        if(context.performed){
+        if(context.performed && isGrounded()){
             _rigidbody.velocity = new Vector2(xspeed,jump);
         }
     }
