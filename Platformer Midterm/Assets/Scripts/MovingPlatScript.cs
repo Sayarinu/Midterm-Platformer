22c41b8f8ad0597584eab70d747ae806c31b9d6e
Jorge-A-Velasquez <jav494@nyu.edu>
2022-10-25 18:52:45 -0400

    created a moving platform

--- Platformer Midterm/Assets/Scripts/MovingPlatScript.cs
+++ Platformer Midterm/Assets/Scripts/MovingPlatScript.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class MovingPlatScript : MonoBehaviour
 {
+    [SerializeField]
+    float speed = 1f;
+    [SerializeField]
+    float range = .1f;
+    [SerializeField]
+    GameObject points;
+    [SerializeField]
+    GameObject platform;
+    List<Vector3> waypointsList;
+    Vector3 currTarget;
+    int currInd = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        waypointsList = new List<Vector3>();
+        foreach (Transform c in points.transform) {
+            waypointsList.Add(c.position);
+        }
+        currTarget = waypointsList[currInd];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        platform.transform.position = Vector3.MoveTowards(platform.transform.position, currTarget, speed * Time.deltaTime);
+        if (Vector3.Distance(currTarget, platform.transform.position) <= range) {
+            currInd++;
+            if (currInd >= waypointsList.Count) {
+                currInd = 0;
+            }
+            currTarget = waypointsList[currInd];
+        }
     }
 }
